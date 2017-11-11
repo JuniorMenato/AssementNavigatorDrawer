a41b950b8905f01b407790e53b9dabdfbf785720
JuniorMenato <menatinho@globomail.com>
2017-11-11 13:12:55 -0200

    Initial commit

---
+++ app/src/main/java/com/example/junio/assementnavigatordrawer/Cursos.java
@@ -0,0 +1,26 @@
+package com.example.junio.assementnavigatordrawer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by junio on 02/10/2017.
+ */
+
+public class Cursos extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.cursos , container , false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Cursos");
+    }
+}
